3efd6ce83c53275a011b4d77be754b2b4602792d
lakhwinder <lakhwinder101199@gmail.com>
2023-10-19 17:14:21 +0530

    new_changes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import { CartProvider } from './context/cart_context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const domain = process.env.REACT_APP_AUTH_DOMAIN;
-const clientId = process.env.REACT_APP_CLIENT_ID;
 root.render(
   <AppProvider>
     <FilterContextProvider>
